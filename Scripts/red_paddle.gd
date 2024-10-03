faa042aff573801d61935bcfdc5edba0b05a909a
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-10-03 17:11:41 +0100

    Updated opp script to ai player

--- Scripts/red_paddle.gd
+++ Scripts/red_paddle.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 600
+enum OP_TYPE {
+	SELF,
+	AI,
+}
+
+var speed = 600
+var ball
+
+func _ready() -> void:
+	ball = get_parent().find_child("ball")
 
 func _physics_process(delta: float) -> void:
-	var paddle_velocity = Vector2.ZERO
+	move_and_collide(Vector2(0, get_opp_direction()) * speed * delta)
 	
-	if Input.is_action_pressed("arrowUp"):
-		paddle_velocity.y -= SPEED
-	if Input.is_action_pressed("arrowDown"):
-		paddle_velocity.y += SPEED
+	#var paddle_velocity = Vector2.ZERO
+	#
+	#if Input.is_action_pressed("arrowUp"):
+		#paddle_velocity.y -= SPEED
+	#if Input.is_action_pressed("arrowDown"):
+		#paddle_velocity.y += SPEED
+	#
+	#move_and_collide(paddle_velocity * delta)
 	
-	move_and_collide(paddle_velocity * delta)
+func get_opp_direction():
+	if abs(ball.position.y - position.y) > 25:
+		if ball.position.y > position.y: return 1
+		else: return -1
+	else: return 0

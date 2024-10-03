b9e805eea4f959dad561d7274aa35150e56309a1
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-10-03 18:07:25 +0100

    Added Scoring system

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-var ball
+var blue_score = 0
+var red_score = 0
 
-func _on_red_goal_body_entered(body: Node2D) -> void:
+func _on_red_goal_body_entered(_body: Node2D) -> void:
 	$ball.position = Vector2(640, 360)
 	$ball.speed = 400
+	blue_score +=1
+	$CanvasLayer/blue_score.text = str(blue_score)
 
-func _on_blue_goal_body_entered(body: Node2D) -> void:
+func _on_blue_goal_body_entered(_body: Node2D) -> void:
 	$ball.position = Vector2(640, 360)
 	$ball.speed = 400
+	red_score +=1
+	$CanvasLayer/red_score.text = str(red_score)

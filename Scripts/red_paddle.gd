2ee63e7f2c7acaf3b89422faff5b5af046ded100
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-10-03 16:27:03 +0100

    Added opp mvt and field bdrt collisions

--- Scripts/red_paddle.gd
+++ Scripts/red_paddle.gd
@@ -5,9 +5,9 @@ const SPEED = 600
 func _physics_process(delta: float) -> void:
 	var paddle_velocity = Vector2.ZERO
 	
-	if Input.is_action_pressed("keyUp"):
+	if Input.is_action_pressed("arrowUp"):
 		paddle_velocity.y -= SPEED
-	if Input.is_action_pressed("keyDown"):
+	if Input.is_action_pressed("arrowDown"):
 		paddle_velocity.y += SPEED
 	
 	move_and_collide(paddle_velocity * delta)
